6cdbf587ff6cfad4cc071aad8862ef96e343b61b
Catalin Sajin <catalin.sajin@studio.unibo.it>
2023-01-12 12:33:01 +0100

    clean a lot of things, add lambda directory

--- Algorithms/Modifying/transform.cpp
+++ Algorithms/Modifying/transform.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cctype>
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -14,25 +13,33 @@ void print_ordinals(std::vector<std::size_t> const &ordinals) {
 
 int main() {
   std::string s{"hello"};
-  std::transform(s.cbegin(), s.cend(),
+  std::transform(s.begin(), s.end(),
                  s.begin(), // write to the same location
                  [](unsigned char c) { return std::toupper(c); });
   std::cout << "s = " << quoted(s) << '\n';
 
-  // achieving the same with std::for_each (see Notes above)
+  // achieving the same with std::for_each
   std::string g{"hello"};
-  std::for_each(g.begin(), g.end(),
-                [](char &c) // modify in-place
-                { c = std::toupper(static_cast<unsigned char>(c)); });
+  std::for_each(g.begin(), g.end(), [](char &c) {
+    c = std::toupper(static_cast<unsigned char>(c));
+  });
   std::cout << "g = " << quoted(g) << '\n';
 
+  // creo un vettore che contiene gli integer dei caratteri di Hello; il back
+  // inserter ci deve essere perché non metto la size che ha ordinals, e se
+  // metto al suo posto ordinals.begin(), mi dà segmentation fault, poiché
+  // ordinals non ha capacity.
   std::vector<std::size_t> ordinals;
-  std::transform(s.cbegin(), s.cend(), std::back_inserter(ordinals),
+  std::transform(s.begin(), s.end(), std::back_inserter(ordinals),
                  [](unsigned char c) { return c; });
 
   print_ordinals(ordinals);
 
-  std::transform(ordinals.cbegin(), ordinals.cend(), ordinals.cbegin(),
+  // transform può anche essere usata su due range (se uso una funzione che
+  // prende due parametri e non uno). qui prendo ordinals e sovrascrivo il
+  // doppio di ogni elemento di ordinals; terzo argomento -> inizio secondo
+  // range; quarto argomento -> inizio range d'arrivo
+  std::transform(ordinals.begin(), ordinals.end(), ordinals.begin(),
                  ordinals.begin(), std::plus<>{});
 
   print_ordinals(ordinals);
---
+++ Lambda/Lambda.cpp
@@ -0,0 +1,19 @@
+/*class Classe_equivalente_lambda {
+    int n_;
+
+   public:
+    explicit Classe_equivalente_lambda(int n) : n_{n} {};
+    bool operator()(int comparison_value) { return comparison_value < n_; }
+};*/
+
+int main() {
+  int comparison_value = 10;
+  auto lambda = [comparison_value](int n) { return comparison_value < n; };
+  return lambda(13);
+
+  // è la stessa cosa dello scrivere :
+  /*
+  Classe_equivalente_lambda c{comparison_value};
+  c(13);
+  */
+}
